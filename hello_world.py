9c8675ac943ca1470254ad0b26ff40bd85bf111d
gamagegit <cgamage92@yahoo.com>
2022-06-06 16:24:38 -0700

    hello world

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World")  #1
+
+name = "Chamith"
+print("Hello ", name)  #2a
+
+print("Hello "+ name)  #2b
+
+num = 5
+print("Hello", num)    #3a
+print("Hello "+ str(num))  #3b
+
+food_one = "pizza"
+food_two = "tacos"
+
+print("I love to eat "+ food_one + " and " +food_two )  #4a
+print('I love to eat {food_one} and {food_two}.')  #4b
